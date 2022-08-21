e7137b7249197ee2e5d526b2ee2e7a9a97299f99
lemon <1498737640@qq.com>
2022-08-21 15:14:31 +0800

    添加自动更新项目

---
+++ NPhoenixAutoUpdateTool/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NPhoenixAutoUpdateTool
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+      if(e.Args.Length == 0)
+      {
+        this.Shutdown();
+      }
+
+      if(Regex.IsMatch(e.Args[0], "^(((ht|f)tp[s]?):\\/\\/)?([^!@#$%^&*?.\\s-]([^!@#$%^&*?.\\s]{0,63}[^!@#$%^&*?.\\s])?\\.)+[a-z]{2,6}\\/?"))
+      {
+        Global.UpdateUrl = e.Args[0];
+        MessageBox.Show(Global.UpdateUrl);
+      }
+
+      this.Shutdown();
+    }
+  }
+}
---
+++ NPhoenixAutoUpdateTool/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPhoenixAutoUpdateTool
+{
+    public static class Global
+    {
+      public static string UpdateUrl { get; set; }
+    }
+}
