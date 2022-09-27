8d4ca439b3de795e3f090c22fa61d67ce845f381
qiuwei <bruce.qiu@meetfuture.com>
2022-09-27 12:00:38 +0800

    add icons and mvp.svp font

---
+++ LeagueOfLegendsBoxer/UserControls/MVP.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace LeagueOfLegendsBoxer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MVP.xaml
+    /// </summary>
+    public partial class MVP : UserControl
+    {
+        public static DependencyProperty MyFontSizeProperty = DependencyProperty.Register("MyFontSize",typeof(double),typeof(MVP));
+        public double MyFontSize
+        {
+            get { return (double)GetValue(MyFontSizeProperty); }
+            set { SetValue(MyFontSizeProperty, value); }
+        }
+
+        public MVP()
+        {
+            InitializeComponent();
+            txt.SetBinding(FontSizeProperty, new Binding("MyFontSize") { Source = this });
+        }
+    }
+}
---
+++ LeagueOfLegendsBoxer/UserControls/SVP.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace LeagueOfLegendsBoxer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SVP.xaml
+    /// </summary>
+    public partial class SVP : UserControl
+    {
+        public static DependencyProperty MyFontSizeProperty = DependencyProperty.Register("MyFontSize", typeof(double), typeof(MVP));
+        public double MyFontSize
+        {
+            get { return (double)GetValue(MyFontSizeProperty); }
+            set { SetValue(MyFontSizeProperty, value); }
+        }
+
+        public SVP()
+        {
+            InitializeComponent();
+            txt.SetBinding(FontSizeProperty, new Binding("MyFontSize") { Source = this });
+        }
+    }
+}
